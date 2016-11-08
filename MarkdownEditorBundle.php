52083a73447fbb0db23fb3f52d6db3813ca4e38c
Rafal Przetakowski <Rafal.Przetakowski@grupawp.pl>
2016-11-08 08:03:05 +0100

    Bundle c.d

---
+++ MarkdownEditorBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author        Rafal Przetakowski <rafal.przetakowski@grupawp.pl>
+ * @copyright (c) Grupa WIrtualna Polska S.A.
+ *
+ */
+
+namespace Beeflow\MarkdownEditorBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MarkdownEditorBundle extends Bundle
+{
+
+}
